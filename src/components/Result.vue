c239bb3fe984b1531ff502091fb06554a4e10088
whitechiina <1293616053@qq.com>
2026-01-22 10:42:22 +0800

    fix: 去掉删除

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -8,14 +8,12 @@
     <template #header>
       <div class="dialog-title">
         <span class="text-2xl font-bold">抽奖结果</span>
-        <span class="text-sm text-gray-500 ml-2.5">(点击号码可以删除)</span>
       </div>
     </template>
     <div
       v-for="(item, index) in resultList"
       :key="index"
       class="listrow flex leading-8 mb-4"
-      @click="(event) => deleteRes(event, item)"
     >
       <span class="name w-[100px] font-bold">
         {{ item.name }}
@@ -25,7 +23,7 @@
           暂未抽奖
         </span>
         <span
-          class="card inline-block px-1.5 leading-8 text-center text-lg font-bold rounded border border-gray-300 bg-gray-100 relative cursor-pointer hover:before:content-['删除'] hover:before:w-full hover:before:h-full hover:before:bg-gray-300 hover:before:absolute hover:before:left-0 hover:before:top-0 hover:before:text-red-500"
+          class="card inline-block px-1.5 leading-8 text-center text-lg font-bold rounded border border-gray-300 bg-gray-100 relative cursor-pointer"
           v-for="(data, j) in item.value"
           :key="j"
           :data-res="data"
